9847b57b984272034fa7636da3f4fde827010566
Caelvanost <caelvanost@gmail.com>
2026-08-15 23:39:51 +0200

    Validate STR proxies through official native API

--- src/ProxyResolver.h
+++ src/ProxyResolver.h
@@ -4,8 +4,17 @@
 
 namespace IEDSyncTogether
 {
+    using RemoteProxyScanCallback = std::function<void(std::vector<RE::FormID>)>;
+
     bool EqualsInsensitive(std::string_view left, std::string_view right);
-    bool IsLikelyRemotePlayerProxy(RE::Actor* actor);
+
+    // Ask STR itself whether the dynamic actors currently present are remote
+    // players. SkyrimTogetherUtils.IsRemotePlayer is an official STR 1.8.0
+    // native Papyrus function and is the authority for this classification.
+    bool RequestRemotePlayerProxyScan(RemoteProxyScanCallback callback);
+    void ClearRemotePlayerProxyCache();
+
+    [[nodiscard]] bool IsVerifiedRemotePlayerProxy(RE::Actor* actor);
     std::vector<RE::Actor*> FindRemotePlayerProxies();
     RE::Actor* FindRemotePlayerProxy(std::string_view playerName);
 }
