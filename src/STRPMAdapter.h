ea58eb86e459c51f989612a897f739bce8c853bd
Caelvanost <caelvanost@gmail.com>
2026-08-19 20:30:57 +0200

    Add pending resend and heartbeat state

--- src/STRPMAdapter.h
+++ src/STRPMAdapter.h
@@ -14,17 +14,30 @@ namespace IEDSyncTogether
 
         bool Start();
         void Stop();
+        void Reset();
         void Publish(const LocalIEDState& state, std::string_view payload);
 
     private:
         static void STRPM_CALL OnMessage(const STRPM::Message* message, void* userData);
         bool LoadApi();
         std::string GetLocalPlayerName() const;
+        void RetryLoop(std::stop_token token);
+        void RetryTick();
+        bool TrySend(const LocalIEDState& state, std::string_view payload, std::string_view reason);
 
         std::atomic_bool _running{ false };
         const STRPM::Interface* _api{ nullptr };
         STRPM::ListenerHandle _listener{};
         STRPM::ConnectionID _localConnectionID{ 0 };
         std::mutex _sendMutex;
+
+        mutable std::mutex _stateMutex;
+        LocalIEDState _latestState;
+        std::string _latestPayload;
+        bool _pending{ false };
+        bool _haveSuccessfulSend{ false };
+        std::optional<STRPM::Result> _lastFailure;
+        std::chrono::steady_clock::time_point _lastSuccessfulSend{};
+        std::jthread _retryTimer;
     };
 }
